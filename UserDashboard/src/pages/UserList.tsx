84ac8efedceccdd213e268273d161784ba3161b9
AnnEldho <annmaryeldho23@gmail.com>
2026-06-03 21:05:00 +0530

    feat: implement user search

--- UserDashboard/src/pages/UserList.tsx
+++ UserDashboard/src/pages/UserList.tsx
@@ -1,30 +1,57 @@
 import { useEffect, useState } from "react";
 import { getUsers } from "../services/UserServices";
-import type{ User } from "../types/User";
+import type { User } from "../types/User";
 import UserCard from "../components/UserCard";
 
 function UserList() {
   const [users, setUsers] = useState<User[]>([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getUsers().then((data) => setUsers(data));
   }, []);
 
+  const filteredUsers: User[] = [];
+
+  for (const user of users) {
+    if (
+      user.name.toLowerCase().includes(search.toLowerCase()) ||
+      user.username.toLowerCase().includes(search.toLowerCase()) ||
+      user.email.toLowerCase().includes(search.toLowerCase())
+    ) {
+      filteredUsers.push(user);
+    }
+  }
+
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-  <h1 className="text-4xl font-bold mb-8">
-    User Dashboard
-  </h1>
+      <h1 className="text-4xl font-bold mb-8">
+        User Dashboard
+      </h1>
 
-  <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-    {users.map((user) => (
-      <UserCard
-        key={user.id}
-        user={user}
+      <input
+        type="text"
+        placeholder="Search by name, username or email..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="w-full p-3 mb-6 border rounded-lg bg-white"
       />
-    ))}
-  </div>
-</div>
+
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map((user) => (
+            <UserCard
+              key={user.id}
+              user={user}
+            />
+          ))
+        ) : (
+          <p className="text-lg font-semibold">
+            No users found
+          </p>
+        )}
+      </div>
+    </div>
   );
 }
 
